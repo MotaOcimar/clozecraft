658c7571a7d43ae620fa75c4a99c6b68b60445b8
Ocimar (Windows) <ocimarmota13@gmail.com>
2023-10-22 22:19:28 -0300

    Inicia os testes

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { ClozeNoteClassic } from "./src/implementation/ClozeNoteClassic"
+import { ClozeNoteSimple } from "./src/implementation/ClozeNoteSimple"
+import { ClozeNoteOL } from "./src/implementation/ClozeNoteOL"
+import { ClozePattern } from "./src/implementation/ClozePattern"
+
+
+export {
+    ClozeNoteClassic,
+    ClozeNoteSimple,
+    ClozeNoteOL,
+    ClozePattern
+}
--- tests/clozePattern.test.ts
+++ tests/clozePattern.test.ts
@@ -1,66 +1,19 @@
-import { ClozePattern } from "../src/implementation/ClozePattern";
-import { IClozeRegExpExecArray } from "../src/interfaces/IClozeRegExpExecArray";
+import { ClozePattern } from '../src/implementation/ClozePattern';
 
-let patterns =
-"[answer][%%\\[123\\]%%][^\\[hint\\]]\n\
-{{[c123::]answer[::hint]}}\n\
-*[c123::][answer][::hint]*\n\
-*[hint::][answer][::123]*";
+test('ClozePattern constructor SHOULD throw an error WHEN \
+the pattern is missing the cloze sequence number', () => {
+    const patternWithoutClozeSeqNumber = '{{answer[::hint]}}';
+    expect(() => new ClozePattern(patternWithoutClozeSeqNumber)).toThrowError();
+});
 
-let clozeOLExample =
-"[oi mundo]%%[as]%%^[dica1] \
-{{cas::oi mundo::dica1}} \
-*dica1::[oi mundo]::as* \
-*dica2::[texto2]::hsa* \
-*cas::[oi mundo]::dica1*";
+test('ClozePattern constructor SHOULD throw an error WHEN \
+the pattern is missing the answer', () => {
+    const patternWithoutAnswer = '{{[c123::][::hint]}}';
+    expect(() => new ClozePattern(patternWithoutAnswer)).toThrowError();
+});
 
-let clozeClassicExample =
-"[oi mundo]%%[1]%%^[dica1] \
-{{c2::oi mundo::dica1}} \
-*dica1::[oi mundo]::3* \
-*dica2::[texto2]::4* \
-*c5::[oi mundo]::dica1*";
-
-let clozeSimpleExample = 
-"[texto1]^[dica1] \
-{{texto2::dica2}} \
-*dica3::[texto3]* \
-*dica4::[texto4]* \
-*[texto5]::dica5*";
-
-
-patterns.split("\n").forEach((line) => {
-    let clozePattern = new ClozePattern(line);
-
-    console.log("\n\n")
-    console.log("pattern: " + line);
-    console.log(clozePattern.clozeSimpleRegex);
-    console.log(clozePattern.clozeClassicRegex);
-    console.log(clozePattern.clozeOLRegex);
-
-    let match: IClozeRegExpExecArray | null;
-    // while (match = clozePattern.clozeOLRegex.exec(clozeOLExample)) {
-    //     console.log(clozePattern.clozeOLRegex)
-    //     console.log("clozeText: " + match.clozeText);
-    //     console.log("clozeHint: " + match.clozeHint);
-    //     console.log("clozeSeq: " + match.clozeSeq);
-    // }
-
-    // while (match = clozePattern.clozeClassicRegex.exec(clozeClassicExample)) {
-    //     console.log(clozePattern.clozeClassicRegex)
-    //     console.log("clozeText: " + match.clozeText);
-    //     console.log("clozeHint: " + match.clozeHint);
-    //     console.log("clozeSeq: " + match.clozeSeq);
-    // }
-
-    const clozeRegex = clozePattern.clozeSimpleRegex;
-    console.log(clozeRegex);
-    while (match = clozeRegex.exec(clozeSimpleExample)) {
-        console.log("match: " + match);
-        console.log("cloze raw: " + match.answer)
-        console.log("cloze answer: " + match.answer);
-        console.log("cloze hint: " + match.hint);
-        console.log("cloze seq: " + match.seq);
-    }
-
+test('ClozePattern constructor SHOULD throw an error WHEN \
+the pattern is missing the hint', () => {
+    const patternWithoutHint = '{{[c123::]answer}}';
+    expect(() => new ClozePattern(patternWithoutHint)).toThrowError();
 });
